3bd6c69356f6282ada5bdbf73c3ab90046d76ca0
Florian Vichot <florian.vichot@gmail.com>
2016-03-22 20:54:03 +0100

    WIP

---
+++ README.md
@@ -0,0 +1,3 @@
+Quick API demo for Stanley Robotics
+
+https://www.ssllabs.com/ssltest/analyze.html?d=ec2.flo.click&hideResults=on
--- python/api.py
+++ python/api.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
+from flask_restful import Resource, Api
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+app = Flask(__name__)
+api = Api(app)
+
+todos = {}
+
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
+api.add_resource(TodoSimple, '/<string:todo_id>')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
